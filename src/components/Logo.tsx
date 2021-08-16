7a4899bd8cc90a28a0e4fa02e4690c87e03bf47b
ViniFarias <vinifarias.vf@gmail.com>
2021-08-16 16:59:02 -0300

    feat: add Logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+import { Flex, Text, Spacer } from '@chakra-ui/react'
+import { BiCoffee } from 'react-icons/bi'
+
+const Logo = () => {
+  return (
+    <Flex paddingX={4} align="center" color="secondary.700">
+      <BiCoffee size={35}/>
+      <Text fontSize="2xl" fontFamily="Lexend Deca">NaMesa</Text>
+    </Flex>
+  )
+}
+
+export default Logo
