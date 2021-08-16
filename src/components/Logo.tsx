3081ecdd1aef4f8fb4fbfd2c5579bc1e2f34655a
ViniFarias <vinifarias.vf@gmail.com>
2021-08-16 17:00:03 -0300

    chore(Logo): fix format

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,8 +4,10 @@ import { BiCoffee } from 'react-icons/bi'
 const Logo = () => {
   return (
     <Flex paddingX={4} align="center" color="secondary.700">
-      <BiCoffee size={35}/>
-      <Text fontSize="2xl" fontFamily="Lexend Deca">NaMesa</Text>
+      <BiCoffee size={35} />
+      <Text fontSize="2xl" fontFamily="Lexend Deca">
+        NaMesa
+      </Text>
     </Flex>
   )
 }
