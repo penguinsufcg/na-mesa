304337a0662ce94c2142e9cb645f1f79302fd9aa
fanny <fanny.vieira@ccc.ufcg.edu.br>
2021-08-16 21:49:43 -0300

    refactor: Improve useEffect performance

--- src/hooks/useFirestoreQuery.ts
+++ src/hooks/useFirestoreQuery.ts
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react'
 import type firebase from 'firebase'
+import { useEffect, useRef, useState } from 'react'
 
-type FirebaseDocRef =
+type FirestoreQuery =
   | firebase.firestore.Query
   | firebase.firestore.DocumentReference
 
@@ -14,36 +14,72 @@ function isDocumentReference(
   return (query.path?.match(/\//g) || []).length % 2 !== 0
 }
 
-function useFirestoreQuery<Entity>(query: FirebaseDocRef): Entity | null {
+function onFirebaseCollectionChange<Entity>(
+  query: firebase.firestore.Query,
+  callback: any,
+) {
+  return query.onSnapshot((snapshot) => {
+    snapshot.docChanges().forEach((change) => {
+      switch (change.type) {
+        case 'added':
+        case 'modified':
+          callback(change.doc.data() as Entity)
+          break
+        case 'removed':
+          callback(null)
+          break
+        default:
+          console.log('[useFirestoreQuery]: Unexpected firestore change')
+          break
+      }
+    })
+  })
+}
+
+function onFirebaseDocChange<Entity>(
+  query: firebase.firestore.DocumentReference,
+  callback: any,
+) {
+  return query.onSnapshot((snapshot) => {
+    callback(snapshot.data() as Entity)
+  })
+}
+
+function useFirestoreQuery<Entity>(query: FirestoreQuery): Entity | null {
   const [doc, setDoc] = useState<Entity | null>(null)
-  if (isDocumentReference(query)) {
-    useEffect(() => {
-      const unsubscribe = query.onSnapshot((snapshot) => {
-        setDoc(snapshot.data() as Entity)
-      })
-      return () => unsubscribe()
-    }, [])
-  } else {
-    useEffect(() => {
-      const unsubscribe = query.onSnapshot((snapshot) => {
-        snapshot.docChanges().forEach((change) => {
-          switch (change.type) {
-            case 'added':
-            case 'modified':
-              setDoc(change.doc.data() as Entity)
-              break
-            case 'removed':
-              setDoc(null)
-              break
-            default:
-              console.log('[useFirestoreQuery]: Unexpected firestore change')
-              break
-          }
-        })
-      })
-      return () => unsubscribe()
-    }, [])
-  }
+  const queryRef = useRef<FirestoreQuery>(query)
+
+  useEffect(() => {
+    if (isDocumentReference(queryRef.current) && isDocumentReference(query)) {
+      if (queryRef.current.isEqual(query)) {
+        queryRef.current = query
+      }
+    } else if (
+      !isDocumentReference(queryRef.current) &&
+      !isDocumentReference(query)
+    ) {
+      if (queryRef.current.isEqual(query)) {
+        queryRef.current = query
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    if (!queryRef.current) {
+      return () => {}
+    }
+
+    let unsubscriber = () => {}
+    if (isDocumentReference(queryRef.current)) {
+      unsubscriber = onFirebaseDocChange<Entity>(queryRef.current, setDoc)
+    } else {
+      unsubscriber = onFirebaseCollectionChange<Entity>(
+        queryRef.current,
+        setDoc,
+      )
+    }
+    return () => unsubscriber()
+  }, [queryRef])
 
   return doc
 }
