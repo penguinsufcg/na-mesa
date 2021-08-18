30624f5fa1e6d80f88f28e68750465bf27aa2da1
Thayanne Sousa <thayannevls@gmail.com>
2021-08-18 18:40:37 -0300

    Add menu component

---
+++ src/components/client/Layout/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Flex, Grid, GridItem } from '@chakra-ui/react'
+import { Header } from './components/Header'
+
+const Layout = ({ children }) => {
+
+  return (
+   <Flex direction="column">
+
+    </Flex>
+  )
+}
+
+export default Layout
