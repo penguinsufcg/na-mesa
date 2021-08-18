32ffb18d3beee17bcf8ae27f42707be53d250fb4
Mateus Oliveira <33286442+mateusoliveira2@users.noreply.github.com>
2021-08-17 21:51:39 -0300

    Initial design system (#8)
    * Initial design system
    
    * Adjust index

---
+++ src/theme/font.ts
@@ -0,0 +1,9 @@
+// Generate color schemes here: https://smart-swatch.netlify.app
+
+const fonts = {
+  heading: "'Lexend Deca', sans-serif'",
+  body: "'Poppins', sans-serif",
+
+}
+
+export default fonts
