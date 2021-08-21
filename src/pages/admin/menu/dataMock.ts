23a01726a5f3123db5f409f6d3261ce70948dc35
ViniFarias <vinifarias.vf@gmail.com>
2021-08-21 14:13:57 -0300

    refact: improve menu admin page code

--- src/pages/admin/menu/dataMock.ts
+++ src/pages/admin/menu/dataMock.ts
@@ -1,12 +1,13 @@
 interface DishType {
   name: string
   description: string
-  time: number
   servings: number
   price: number
   id: string
   imageURL: string
-  [key: string]: string | number // DishType is indexable; not a new property
+  available: boolean
+  preparationTime: number
+  [key: string]: string | number | boolean // DishType is indexable; not a new property
 }
 
 const DATA: DishType[] = [
@@ -14,85 +15,94 @@ const DATA: DishType[] = [
     id: '1',
     name: 'Batata frita',
     description: 'Um prato delicioso',
-    time: 10,
     servings: 2,
     price: 20.0,
     imageURL: '',
+    available: false,
+    preparationTime: 10,
   },
   {
     id: '2',
     name: 'Guacamole',
     description: 'Um prato delicioso',
-    time: 20,
     servings: 4,
     price: 15.0,
     imageURL: '',
+    available: true,
+    preparationTime: 10,
   },
   {
     id: '3',
     name: 'Coxinha',
     description: 'Um prato delicioso',
-    time: 5,
     servings: 1,
     price: 5.0,
     imageURL: '',
+    available: false,
+    preparationTime: 10,
   },
   {
     id: '4',
     name: 'Suco de Uva',
     description: 'Um prato delicioso',
-    time: 3,
     servings: 1,
     price: 2.0,
     imageURL: '',
+    available: true,
+    preparationTime: 10,
   },
   {
     id: '5',
     name: 'Pizza',
     description: 'Um prato delicioso',
-    time: 30,
     servings: 4,
     price: 35.0,
     imageURL: '',
+    available: false,
+    preparationTime: 10,
   },
   {
     id: '6',
     name: 'Quentinhas delicia',
     description: 'Um prato delicioso',
-    time: 10,
     servings: 2,
     price: 20.0,
     imageURL: '',
+    available: true,
+    preparationTime: 10,
   },
   {
     id: '7',
     name: 'Batata frita TOP',
     description: 'Descrição Descrição Descrição Descrição',
-    time: 15,
     servings: 2,
     price: 10.0,
     imageURL:
       'https://escoladereceitas.com/wp-content/uploads/2021/01/como-fazer-batata-frita-crocante-e-sequinha.jpeg',
+    available: false,
+    preparationTime: 10,
   },
   {
     id: '8',
     name: 'Arroz',
     description: 'Descrição Descrição Descrição Descrição',
-    time: 15,
     servings: 2,
     price: 10.0,
     imageURL:
       'https://vivareceita-cdn.s3.amazonaws.com/uploads/2020/11/Aprenda-como-fazer-um-arroz-dos-deuses.-Fonte-Pinterest.jpg',
+    available: true,
+    preparationTime: 10,
   },
   {
     id: '9',
     name: 'Macarrão',
     description: 'Descrição Descrição Descrição Descrição',
-    time: 15,
     servings: 2,
     price: 10.0,
     imageURL:
       'https://www.sabornamesa.com.br/media/k2/items/cache/665e96c29d55b13435d7a8d39deafe53_XL.jpg',
+    available: true,
+    preparationTime: 10,
   },
 ]
 
--- src/pages/admin/menu/index.tsx
+++ src/pages/admin/menu/index.tsx
@@ -1,9 +1,7 @@
 import { Heading, Flex, Spacer, Button, useDisclosure } from '@chakra-ui/react'
 import { useEffect, useState } from 'react'
 import { BiPlus } from 'react-icons/bi'
-import DishTable from '@/components/Admin/Menu/components/DishTable'
-import SearchInput from '@/components/Admin/Menu/components/SearchInput'
-import SelectInput from '@/components/Admin/Menu/components/SelectInput'
+import DishTable from '@/components/Admin/DishTable'
 import DishModal from '@/components/Admin/DishModal'
 
 import DATA from './dataMock'
@@ -11,59 +9,42 @@ import DATA from './dataMock'
 interface DishType {
   name: string
   description: string
-  time: number
   servings: number
   price: number
   id: string
   imageURL: string
-  [key: string]: string | number // DishType is indexable; not a new property
+  available: boolean
+  preparationTime: number
+  [key: string]: string | number | boolean // DishType is indexable; not a new property
 }
 
 const Menu = () => {
-  const [data, setData] = useState<DishType[]>(DATA)
-  const [orderBy, setOrderBy] = useState('')
-  const [searchText, setSearchText] = useState('')
-
-  const { isOpen, onOpen, onClose } = useDisclosure()
-
-  const handleOrderBy = (event: any) => {
-    setOrderBy(event.target.value)
-  }
-
-  const handleSearch = (event: any) => {
-    if (event.key === 'Enter') {
-      setSearchText(event.target.value.toLowerCase())
-    }
-  }
-
-  useEffect(() => {
-    const filtered = [...DATA].filter((dish) =>
-      dish.name.toLowerCase().includes(searchText),
-    )
-    const sorted = [...filtered].sort((a, b) =>
-      b[orderBy] > a[orderBy] ? -1 : 1,
-    )
-    setData(sorted)
-  }, [orderBy, searchText])
+  const {
+    isOpen: isOpenDishModal,
+    onOpen: onOpenDishModal,
+    onClose: onCloseDishModal,
+  } = useDisclosure()
 
   return (
     <Flex width="full" direction="column" padding={12}>
       <Flex paddingBottom={14}>
         <Heading fontWeight="normal">Cardápio</Heading>
         <Spacer />
-        <Button leftIcon={<BiPlus size={20} />} margin={0} onClick={onOpen}>
+        <Button
+          leftIcon={<BiPlus size={20} />}
+          margin={0}
+          onClick={onOpenDishModal}>
           NOVO PRODUTO
         </Button>
       </Flex>
 
-      <DishModal update={false} isOpen={isOpen} onClose={onClose} />
+      <DishModal
+        update={false}
+        isOpen={isOpenDishModal}
+        onClose={onCloseDishModal}
+      />
 
-      <Flex marginBottom={3}>
-        <SearchInput onSearch={handleSearch} />
-        <Spacer />
-        <SelectInput onOrder={handleOrderBy} />
-      </Flex>
-      <DishTable data={data} />
+      <DishTable data={DATA} />
     </Flex>
   )
 }
