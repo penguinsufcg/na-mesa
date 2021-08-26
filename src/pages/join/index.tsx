41ac3cdb97a0c6071df9db52ce21a23ffa8cbb3a
fanny <fanny.vieira@ccc.ufcg.edu.br>
2021-08-26 20:16:40 -0300

    Enhance join screen login

--- src/pages/join/index.tsx
+++ src/pages/join/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import Logo from '@/components/Logo'
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, Button, Flex, Text } from '@chakra-ui/react'
 import JoinTableForm from '@/components/client/JoinTableForm'
 
 const JoinTablePage = () => {
   return (
-    <Flex direction="column" sx={{ alignItems: 'center', padding: 4 }}>
+    <Flex
+      direction="column"
+      sx={{
+        alignItems: 'center',
+        padding: 8,
+      }}>
       <Box sx={{ marginBottom: '50%' }}>
         <Logo />
       </Box>
       <JoinTableForm />
+      <Text fontSize="sm" sx={{ marginTop: '5%', textAlign: 'center' }}>
+        A mesa já está aberta ?{' '}
+        <Button variant="link">Acessar mesa pelo código de acesso </Button>
+      </Text>
     </Flex>
   )
 }
