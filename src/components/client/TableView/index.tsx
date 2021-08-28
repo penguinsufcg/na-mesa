cbab50731f3e4ff18777f88449b3c93bdd8beaa3
fanny <fanny.vieira@ccc.ufcg.edu.br>
2021-08-28 16:30:41 -0300

    Fix button css

--- src/components/client/TableView/index.tsx
+++ src/components/client/TableView/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@chakra-ui/button'
 import { Flex, Spacer, Text } from '@chakra-ui/layout'
 import { Tag } from '@chakra-ui/tag'
+import { fixControlledValue } from 'antd/lib/input/Input'
 import React, { FC } from 'react'
 
 type Props = {
@@ -12,17 +13,28 @@ const TableView: FC<Props> = () => {
 
   return (
     <Flex direction="column" sx={{ alignContent: 'spaceBetween' }}>
-      <Text fontSize="lg" sx={{ padding: 4 }}>
+      <Text fontSize="lg" sx={{ padding: 4, textAlign: 'center' }}>
         Sua mesa está pronta!
       </Text>
       <Text sx={{ padding: 4 }}>
         Copie o código de acesso abaixo para acessar sua mesa de qualquer
         celular!
       </Text>
-      <Text size="lg" sx={{ padding: 4, textAlign: 'center' }}>
+      <Text
+        size="lg"
+        fontSize="xl"
+        color="primary.500"
+        sx={{ padding: 4, textAlign: 'center' }}>
         #2ADE4FS
       </Text>
-      <Button sx={{ alignSelf: 'end' }} onClick={handleSubmit} width="100%">
+      <Button
+        sx={{
+          position: 'fixed',
+          bottom: '3rem',
+          alignSelf: 'center',
+        }}
+        onClick={handleSubmit}
+        width="80%">
         Acessar
       </Button>
     </Flex>
