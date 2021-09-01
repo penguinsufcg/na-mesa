bf8ae08d941f0c8f2be809d1256cbd8b7037857c
fanny <fanny.vieira@ccc.ufcg.edu.br>
2021-09-01 16:14:07 -0300

    Improve design

--- src/pages/join/[code].tsx
+++ src/pages/join/[code].tsx
@@ -1,28 +1,40 @@
+import Logo from '@/components/Logo'
 import { Button } from '@chakra-ui/button'
-import { Flex, Text } from '@chakra-ui/layout'
+import { Box, Flex, Heading, Text } from '@chakra-ui/layout'
 import { useRouter } from 'next/router'
 import React, { FC } from 'react'
+import { BiCopyAlt } from 'react-icons/bi'
+import { useClipboard } from '@chakra-ui/react'
 
 const TableCodePage: FC = () => {
   const router = useRouter()
   const { code } = router.query
+  const { hasCopied, onCopy } = useClipboard(code ? code.toString() : '')
 
   return (
-    <Flex direction="column" sx={{ alignContent: 'spaceBetween' }}>
-      <Text fontSize="lg" sx={{ padding: 4, textAlign: 'center' }}>
+    <Flex
+      direction="column"
+      sx={{ alignContent: 'spaceBetween', alignItems: 'center' }}>
+      <Box sx={{ marginBottom: '50%' }}>
+        <Logo />
+      </Box>
+      <Heading size="lg" color="gray.600" fontWeight="medium" mb="10px">
         Sua mesa está pronta!
-      </Text>
-      <Text sx={{ padding: 4 }}>
+      </Heading>
+      <Text sx={{ padding: 8 }}>
         Copie o código de acesso abaixo para acessar sua mesa de qualquer
         celular!
       </Text>
-      <Text
-        size="lg"
-        fontSize="xl"
-        color="primary.500"
-        sx={{ padding: 4, textAlign: 'center' }}>
-        {`#${code}`}
-      </Text>
+      <Flex sx={{ alignItems: 'center', color: 'primary.500' }}>
+        <Text
+          size="lg"
+          fontSize="xl"
+          color="primary.500"
+          sx={{ textAlign: 'center' }}>
+          {`${code}`}&nbsp;
+        </Text>
+        <BiCopyAlt size="20" />
+      </Flex>
       <Button
         sx={{
           position: 'fixed',
