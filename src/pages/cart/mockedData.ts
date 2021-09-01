7b67aad7fb250f32ef5ab3219f18ee15f52e7b51
ViniFarias <vinifarias.vf@gmail.com>
2021-09-01 11:00:13 -0300

    feat: add cart page

---
+++ src/pages/cart/mockedData.ts
@@ -0,0 +1,82 @@
+type Item = {
+  name: string
+  price: number
+  quantity: number
+  comments: string
+  imageURL: string
+}
+
+export const DATA: Item[] = [
+  {
+    name: 'Batata frita',
+    price: 10.0,
+    quantity: 2,
+    comments: 'Sem queijo parmesão',
+    imageURL:
+      'https://img.itdg.com.br/tdg/images/recipes/000/018/897/164773/164773_original.jpg?w=1200',
+  },
+  {
+    name: 'Pizza delícia',
+    price: 10.0,
+    quantity: 2,
+    comments: 'Com bastante calabresa',
+    imageURL:
+      'https://diariodonordeste.verdesmares.com.br/image/contentid/policy:1.3107806:1625926867/pizza.jpg?f=16x9&h=720&q=0.8&w=1280&$p$f$h$q$w=1385168',
+  },
+  {
+    name: 'Batata frita',
+    price: 10.0,
+    quantity: 2,
+    comments: 'Frita na hora...obrigado, de nada',
+    imageURL:
+      'https://img.itdg.com.br/tdg/images/recipes/000/018/897/164773/164773_original.jpg?w=1200',
+  },
+  {
+    name: 'Torre de cerveja 10L',
+    price: 100.5,
+    quantity: 3,
+    comments: 'Com balde de gelo GRANDE, por favor',
+    imageURL:
+      'https://http2.mlstatic.com/D_NQ_NP_647864-MLB32282771832_092019-O.jpg',
+  },
+  {
+    name: 'Porção de tripa pequena',
+    price: 10.0,
+    quantity: 2,
+    comments: 'Bem sequinha, viu moço',
+    imageURL:
+      'https://pigz.app/img/upload/products/Tripa-500px-6040e97fe6c0b.jpeg',
+  },
+  {
+    name: 'Porção de ovo de codorna',
+    price: 250.5,
+    quantity: 2,
+    comments: 'Top de verdade',
+    imageURL:
+      'https://alavoura.com.br/wp-content/uploads/2020/04/ovos-de-codorna-1280x720-e1587578125462.jpg',
+  },
+  {
+    name: 'Batata frita',
+    price: 10.0,
+    quantity: 2,
+    comments: 'Top de verdade',
+    imageURL:
+      'https://img.itdg.com.br/tdg/images/recipes/000/018/897/164773/164773_original.jpg?w=1200',
+  },
+  {
+    name: 'Pizza delícia',
+    price: 10.0,
+    quantity: 2,
+    comments: 'Top de verdade',
+    imageURL:
+      'https://diariodonordeste.verdesmares.com.br/image/contentid/policy:1.3107806:1625926867/pizza.jpg?f=16x9&h=720&q=0.8&w=1280&$p$f$h$q$w=1385168',
+  },
+  {
+    name: 'Pizza delícia',
+    price: 10.0,
+    quantity: 2,
+    comments: 'Top de verdade',
+    imageURL:
+      'https://diariodonordeste.verdesmares.com.br/image/contentid/policy:1.3107806:1625926867/pizza.jpg?f=16x9&h=720&q=0.8&w=1280&$p$f$h$q$w=1385168',
+  },
+]
