e9c311d77f59367c939762c34648d764f722a666
ViniFarias <vinifarias.vf@gmail.com>
2021-09-05 23:00:39 -0300

    feat(dish-details): hide form when user is not logged

--- src/pages/dish-details/[dishId].tsx
+++ src/pages/dish-details/[dishId].tsx
@@ -7,12 +7,14 @@ import DishDetails from '@/components/client/DishDetails.tsx'
 import { useRouter } from 'next/router'
 import useMinicart from '@/hooks/useMinicart'
 import { useFirestoreObjectQuery } from '@/hooks/useFirestoreObjectQuery'
+import useSession from '@/hooks/useSession'
 
 const DishPage = () => {
   const router = useRouter()
   const { dishId } = router.query
   const { data: dishData } = useFirestoreObjectQuery<Dish>(`dishes/${dishId}`)
   const { addItem } = useMinicart()
+  const { isLogged } = useSession()
 
   const toast = useToast()
 
@@ -55,7 +57,7 @@ const DishPage = () => {
         <ChevronLeftIcon />
       </Button>
       {dishData && <DishDetails dish={dishData} />}
-      <AddDishForm onSubmit={handleSubmit} />
+      {isLogged && <AddDishForm onSubmit={handleSubmit} />}
     </Flex>
   )
 }
